83c5a27fa75df3abe607580d8377c2a2331cd5e9
D21IT168 <89838129+D21IT168@users.noreply.github.com>
2022-09-19 12:41:46 +0530

    commit 6

--- src/main/webapps/src/App.js
+++ src/main/webapps/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import NavigationBar from "./component/NavigationBar"
 import Footer from "./component/Footer";
+import Regform from './component/Regform';
+
 function App() {
   return (
     <div className="App">
         <NavigationBar/>
+        <Regform/>
         <Footer />
     </div>
   );
